b0aeff68398261ddbe35632e5c04c4c10512fed1
BThomas <bran_thom@yahoo.com>
2023-07-28 13:45:38 -0500

    contact me animation added

--- main.js
+++ main.js
@@ -3,3 +3,11 @@
 window.addEventListener('scroll',()=>{
     document.querySelector('nav').classlist.toggle('window-scrolled', window.scrollY > 0);
 })
+
+// Contact button (circular text buttons)
+
+const textButton = document.querySelectorAll('.contact_btn');
+
+textButton.forEach(textButton => {
+    let text = textButton.querySelector('p');
+    text.innerHTML = text.innerHTML.split('').map((character,index) => `<span style = "transform: rotate(${index * 12}deg">${character}</span>`).join("")});
